e7eb6ce8f4dc040fb79d893b151d9c086e3fbfda
= <aanilsakaryali@gmail.com>
2019-09-25 19:47:37 +0300

    Initial commit

---
+++ backend/src/main/java/com/uberaemos/usermanagementpage/UsermanagementpageApplication.java
@@ -0,0 +1,13 @@
+package com.uberaemos.usermanagementpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementpageApplication.class, args);
+	}
+
+}
